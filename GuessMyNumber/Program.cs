bb5581cade7461a61e5c08be86c83d406bc6b9b3
phildh89 <phildh89@gmail.com>
2020-03-13 13:44:36 -0700

    Update Bisectional Alg

--- GuessMyNumber/Program.cs
+++ GuessMyNumber/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GuessMyNumber
 {
@@ -9,6 +10,10 @@ namespace GuessMyNumber
             PickOptions();
 
         }
+        static int[] StartingArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        static int count = 1;
+
+
         public static void PickOptions()
         {
             Console.WriteLine("\n\tEnter a number to choose your game:");
@@ -16,59 +21,125 @@ namespace GuessMyNumber
             Console.WriteLine("\n\t\t2. Try to find the number between 1 and 1,000");
             Console.WriteLine("\n\t\t3. You guide the computer to find your number between 1 and 100");
 
-            int input = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
 
-            if (input > 3 || input < 1)
+                while (input > 3 || input < 1)
+                {
+                    Console.WriteLine("Invalid entry. Choose a number between 1 and 3.");
+                    input = Convert.ToInt32(Console.ReadLine());
+                }
+
+                if (input == 1)
+                {
+                    Console.Clear();
+                    Console.Write("\n\tEnter a number between 1 and 10: ");
+                    int userNum = Convert.ToInt32(Console.ReadLine());
+                    while (userNum > 10 || userNum < 1)
+                    {
+                        Console.Write("\tInvalid entry. Enter a number between 1 and 10: ");
+                        userNum = Convert.ToInt32(Console.ReadLine());
+                    }
+                    BisectionAlg(StartingArr, userNum);
+                }
+                else if (input == 2)
+                {
+                    UserGuess();
+                }
+                else if (input == 3)
+                {
+                    Console.Clear();
+                    Console.Write("\n\tSelect a number between 1 and 100 for the computer to guess: ");
+                    int userNum = Convert.ToInt32(Console.ReadLine());
+                    while (userNum > 100 || userNum < 1)
+                    {
+                        Console.Write("Invalid entry. Try Again: ");
+                        userNum = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    //create array for 100 nums
+                    int[] CompGuessArr = new int[100];
+                    for (int i = 1; i < 101; i++)
+                    {
+                        CompGuessArr[i - 1] = i;
+                    }
+                    ComputerGuess(CompGuessArr, userNum);
+                }
+        }
+            catch (Exception)
             {
-                Console.WriteLine("Invalid entry. Choose a number between 1 and 3.");
+                Console.Clear();
+                Console.WriteLine("Invalid entry. Try again.");
                 PickOptions();
-            }
-            else if (input == 1)
+    }
+}
+        public static int ArrAverage(int[] numArr)
+        {
+            int arrSum = 0;
+            foreach (var i in numArr)
             {
-                BisectionAlg();
+                arrSum += i;
             }
-            else if (input == 2)
+            return arrSum/numArr.Length;
+        }
+        public static void PrintArr(int[] numArr)
+        {
+            Console.Write("{ ");
+
+            foreach (var i in numArr)
             {
-                UserGuess();
+                Console.Write($"{i} ");
             }
-            else if (input == 3)
-            {
-                ComputerGuess();
-            }
+
+            Console.WriteLine("}");
 
         }
-        public static void BisectionAlg()
+        public static int FindIndex(int arrElement ,int[] numArr)
         {
-            Console.Clear();
-            int[] numArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            Console.Write("\n\tEnter a number between 1 and 10: ");
-            int input = Convert.ToInt32(Console.ReadLine());
-            while (input > 10 || input < 1)
+            for (int i = 0; i < numArr.Length; i++)
             {
-                Console.Write("\tInvalid entry. Enter a number between 1 and 10: ");
-                input = Convert.ToInt32(Console.ReadLine());
-            }
-
-            int CompGuess = numArr.Length / 2;
-            int temp = CompGuess / 2;
-
-            while (input != CompGuess)
-            {
-                if (input > CompGuess)
+                if (numArr[i] == arrElement)
                 {
-                    Console.WriteLine($"My guess is {CompGuess}, which is too low.");
-                    CompGuess += temp;
-                    if (temp != 1) temp /= 2;
-                }
-                else if (input < CompGuess)
-                {
-                    Console.WriteLine($"My guess is {CompGuess}, which is too high.");
-                    CompGuess -= temp;
-                    if (temp != 1) temp /= 2;
+                    return i;
                 }
             }
+            return -1;//Place holder to return error.
+        }
+        public static void BisectionAlg(int[] numArr,int input)
+        {
+
+            PrintArr(numArr);
+            int compGuess = ArrAverage(numArr);
+            int[] tempArr = new int[input > compGuess ? numArr.Length - FindIndex(compGuess,numArr) -1: FindIndex(compGuess, numArr)];
+            
+            if (input > compGuess)
+            {
+                Console.WriteLine($"My guess is {compGuess}, which is too low.\n");
+                int arrIndex = FindIndex(compGuess,numArr);
+                for (int i = 0; i < tempArr.Length; i++)
+                {
+                    tempArr[i] = numArr[arrIndex+1];
+                    arrIndex++;
+                }
+                BisectionAlg(tempArr,input); //recursion
+
+            }
+            else if (input < compGuess)   
+            {
+                Console.WriteLine($"My guess is {compGuess}, which is too high.\n");
+                int arrIndex = FindIndex(compGuess, numArr);
+                for (int i = 0; i < arrIndex; i++)
+                {
+                    tempArr[i] = numArr[i];
+                }
+                BisectionAlg(tempArr, input); //recursion
+            }
+            
             Console.WriteLine($"{input} is the number!");
-
+            Console.WriteLine("\nPress Enter to Continue");
+            Console.ReadKey();
+            PickOptions();
 
         }
         public static void UserGuess()
@@ -100,49 +171,54 @@ namespace GuessMyNumber
 
 
         }
-        public static void ComputerGuess()
+        public static void ComputerGuess(int[] numArr, int input)
         {
-            Console.Clear();
-            Console.Write("\n\tSelect a number between 1 and 100 for the computer to guess: ");
-            int input = Convert.ToInt32(Console.ReadLine());
+            PrintArr(numArr);
 
-            while (input > 100 || input <1)
+            int compGuess = ArrAverage(numArr);
+            int[] tempArr = new int[input > compGuess ? numArr.Length - FindIndex(compGuess, numArr) - 1 : FindIndex(compGuess, numArr)];
+
+            if (input == compGuess)
             {
-                Console.Write("Invalid entry. Try Again: ");
-                input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"\n\t\t{input} is the number! It took me {count} guesses.");
+                Console.WriteLine("\nPress Enter to Continue");
+                Console.ReadKey();
+                PickOptions();
             }
-            int CompGuess = 100 / 2;
-            int temp = CompGuess / 2;
-            int count = 1;
+            Console.WriteLine($"\tComputer guess the number: {compGuess}");
+            Console.WriteLine("\n\tSelect an option:");
+            Console.WriteLine("\t\t1. Guess is too high");
+            Console.WriteLine("\t\t2. Guess is too low");
 
-            while (input != CompGuess)
+            int userNavigation = Convert.ToInt32(Console.ReadLine());
+            while (userNavigation != 1 && userNavigation != 2)
             {
-                Console.WriteLine($"\tComputer guess the number: {CompGuess}");
-                Console.WriteLine("\n\tSelect an option:");
-                Console.WriteLine("\t\t1. Guess is too high");
-                Console.WriteLine("\t\t2. Guess is too low");
-
-                int userNavigation = Convert.ToInt32(Console.ReadLine());
-                while (userNavigation != 1 && userNavigation != 2)
-                {
-                    Console.Write("Invalid input. Try again: ");
-                    userNavigation = Convert.ToInt32(Console.ReadLine());
-                }
-
-                if (userNavigation == 2)
-                {
-                    CompGuess += temp;
-                    if (temp != 1) temp /= 2;
-                    count++;
-                }
-                else if (userNavigation == 1)
-                {
-                    CompGuess -= temp;
-                    if (temp != 1) temp /= 2;
-                    count++;
-                }
+                Console.Write("Invalid input. Try again: ");
+                userNavigation = Convert.ToInt32(Console.ReadLine());
             }
-            Console.WriteLine($"\n\t\t{input} is the number! It took me {count} guesses.");
+
+            if (userNavigation == 2)
+            {
+                int arrIndex = FindIndex(compGuess, numArr);
+                for (int i = 0; i < tempArr.Length; i++)
+                {
+                    tempArr[i] = numArr[arrIndex + 1];
+                    arrIndex++;
+                }
+                count++;
+                ComputerGuess(tempArr, input);
+            }
+            else if (userNavigation == 1)
+            {
+                int arrIndex = FindIndex(compGuess, numArr);
+                for (int i = 0; i < arrIndex; i++)
+                {
+                    tempArr[i] = numArr[i];
+                }
+                count++;
+                ComputerGuess(tempArr, input);
+
+            }
 
         }
     }
